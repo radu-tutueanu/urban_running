263081c1f3b7db0124ebcfdcda518b917192f112
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-02-16 15:02:23 +0200

    more experimenting

---
+++ test/google maps api/calculateroute.js
@@ -0,0 +1,8 @@
+function calcRoute(pointsarray) {
+	if(pointsarray.length < 2 ) {
+			document.write("<h1>too little param</h1>");
+			return;
+	}
+	document.write("<h1>good param</h1>");
+	return;
+}
