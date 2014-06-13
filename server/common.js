09d44992797961487ea7e00b6ee1fa2a09be5baf
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-06-13 23:08:31 +0300

    client-server : login communication

---
+++ server/common.js
@@ -0,0 +1,15 @@
+module.exports = {
+	LOGIN : "login" ,
+	LOGINOK: "login_OK",
+	LOGINNOK: "login_NOK",
+	SEND_ROUTE: "send_route",
+	SEND_ALL_ROUTES: "send_all_routes", // server to 
+	REQUEST_ALL_ROUTES: "request_all_routes",
+	SEND_ROUTE_INFO:"send_route_info"
+
+
+
+
+}
+
+//TODO: this is supposed to be imported by both client and serve. 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var common = require('./common')
 var server;
 
 server = http.createServer(function(req, res){
@@ -9,7 +10,7 @@ server = http.createServer(function(req, res){
     switch (path){
         case '/':
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
+            res.write('<h1>Hello! Try the <a href="/index.html">Main page</a></h1>');
             res.end();
             break;
         case '/index.html':
@@ -42,13 +43,52 @@ io.set('log level', 1);
 
 // define interactions with client
 io.sockets.on('connection', function(socket){
-    //send data to client
+    
     setInterval(function(){
         socket.emit('date', {'date': new Date()});
     }, 1000);
-
     //recieve client data
     socket.on('client_data', function(data){
         process.stdout.write(data.letter);
     });
+    //login
+    socket.on(common.LOGIN, function(data){
+        if ( verifyPass (data.username, data.password )) {
+            socket.emit(common.LOGINOK);
+        }
+        else{
+            socket.emit(common.LOGINNOK);
+        }
+    });
+    //receiveRoute
+
+    //routesRequest
 });
+
+
+
+
+function verifyPass(username, password){
+    console.log( 'verifying pass for client %s', username );
+    return false;
+}
+
+
+function receiveRoute ( atoms ) {
+    coordArray = Array();
+    for (var index in atoms){
+        console.log( atoms[index] );
+        coordArray.push( atoms[index] );
+    }
+    return coordArray;
+}
+
+
+
+/*
+
+
+//send data to client
+    setInterval(function(){
+        socket.emit('date', {'date': new Date()});
+    }, 1000);*/
