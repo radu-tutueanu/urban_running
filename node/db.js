bfcd7d50cd9dbd380ceb20e33d7eecf432cff8d8
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-06-15 17:49:15 +0300

    db: procedures to create ; small fixes

--- node/db.js
+++ node/db.js
@@ -1,13 +1,38 @@
 var mysql = require('db-mysql');
 
+
+
 module.exports = {
   insertUser: function (userName, password, birthDate, city, country) {
      console.log( 'added user' + userName );
      insert('utilizatori', ['user_name', 'parola', 'data_nastere', 'oras', 'tara', 'data_creare'],
       [userName, password, birthDate, city, country, 'sysdate()']);
   },
-  insertTraseu: function () {
+
+  createUser: function ( userName, password, date, city, country){
+
+  request = "call inserare_utilizator(";
+  request = request + wrapWithSingleQuotes( userName ) + ',' 
+              + wrapWithSingleQuotes( password ) + ',' + wrapWithSingleQuotes( date ) 
+              + ',' + wrapWithSingleQuotes( city ) + ',' + wrapWithSingleQuotes( country ) + ');' ;
+  querry( request );
+  },
+
+  insertTraseu: function ( userName, pathName, distance, whenField, whereField, traffic, dogsField, lightsField, securityField, obsField, coordinates ) {
+      
+    
+    request = "call inserare_traseu(";
+    request = request +  wrapWithSingleQuotes( userName ) + ',' +  wrapWithSingleQuotes( pathName ) + ',' +  distance + ',' + wrapWithSingleQuotes( whenField ) + ',' 
+              +  wrapWithSingleQuotes( whereField ) + ',' +  traffic  + ',' +  dogsField  + ',' + lightsField
+              + ',' + wrapWithSingleQuotes( securityField ) + ',' + wrapWithSingleQuotes( obsField );
+    request = addVectorToQuerry( request, coordinates )  + ');';
+    querry( request );
+
+  },
  
+
+  getTrasee: function() {
+
   },
 
   verifyPassword: function () {
@@ -17,10 +42,13 @@ module.exports = {
   a : 'eee'
 };
 
+var MAXCOORDx2 = 20;
 HOSTNAME = 'localhost';
 USERNAME = 'root';
 PASSWORD = 'radu90';
 DATABASE = 'undealergam';
+
+//call inserare_utilizator('buton','passbuton','1995-06-07','Iasi','Romania');
 
 function insert( table, columns, values){
     new mysql.Database({
@@ -36,10 +64,62 @@ function insert( table, columns, values){
 });
 }
 
+function addVectorToQuerry( querry, vector ){
+  var i = 0;
+
+  if (vector.length > 20) {
+    console.error("addVectorToQuerry: too many values " + vector.length) ;
+    return querry;
+  }
+
+  for (; i <= vector.length - 1; i++) {
+    querry = querry + ',' + vector[i];
+  };
+
+  for(;i< MAXCOORDx2 ; i++){
+    querry = querry + ',' + 'null';
+  }
+
+  
+  return querry;
+}
+
 function insertCallback( error, result){
     if (error) {
                         console.log('ERROR: ' + error);
                         return;
                 }
                 console.log('GENERATED id: ' + result.id);
+}
+
+
+
+function querryHandler(error, rows, cols) {
+                if (error) {
+                        console.log('ERROR: ' + error);
+                        //console.log(this.query);
+                        return;
+                }
+                console.log(rows.count + ' ROWS returned');
+}
+
+function wrapWithSingleQuotes( word ){
+  return "'" + word + "'";
+}
+
+
+
+function querry( querryString ) {
+   console.log( "DB querry: " + querryString);
+   new mysql.Database({
+    hostname: HOSTNAME,
+    user: USERNAME,
+    password: PASSWORD,
+    database: DATABASE
+}).connect(function(error) {
+    if (error) {
+        return console.log('CONNECTION error: ' + error);
+    }
+    this.query( querryString ).execute( querryHandler );
+});
 }
--- node/dbusageexample.js
+++ node/dbusageexample.js
@@ -1,4 +1,8 @@
 var db = require('./db');
 
-db.insertUser('andrei', '123123', '1990-04-11 00:00:00', 'Baicoi', 'RO')
-console.log( db.a );
+//db.createUser('andrei20pag', '123123', '1990-04-11 00:00:00', 'Baicoi', 'RO')
+db.insertTraseu('narcis', 'alergare usoara', 22.3, 'seara','trotuar', 2, 4, 4, 'te mananca lupii', 'frumos', [44.41367383, 26.10282897, 44.41367383, 26.10282897, 
+            44.414089110, 26.102625802, 44.414258189, 26.102995271]);
+
+
+  
