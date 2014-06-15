ad161455b8d52c9ca9aa528731f8ded9685a7325
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-06-15 23:58:01 +0300

    index.html: separate css

--- server/server.js
+++ server/server.js
@@ -10,39 +10,49 @@ server = http.createServer(function(req, res){
     var path = url.parse(req.url).pathname;
     switch (path){
         case '/':
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>Hello! Try the <a href="index.html">Main page</a></h1>');
-        res.end();
-        break;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>Hello! Try the <a href="index.html">Main page</a></h1>');
+            res.end();
+            break;
         case '/index.html':
-        fs.readFile(__dirname + htmlDir+ path, function(err, data){
-            if (err){
-                return send404(res);
-            }
-            res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-            res.write(data, 'utf8');
-            res.end();
-        });
-        break;
+            fs.readFile(__dirname + htmlDir+ path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+                res.write(data, 'utf8');
+                res.end();
+            });
+            break;
         case '/adauga_traseu.css':
-        fs.readFile(__dirname + htmlDir+ path, function(err, data){
-            if (err){
-                return send404(res);
-            }
-            res.writeHead(200, {'Content-Type':  'text/css' });
-            res.write(data, 'utf8');
-            res.end();
-        });
-        break;
+            fs.readFile(__dirname + htmlDir+ path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type':  'text/css' });
+                res.write(data, 'utf8');
+                res.end();
+             });
+             break;
+        case '/index.css':
+            fs.readFile(__dirname + htmlDir+ path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type':  'text/css' });
+                res.write(data, 'utf8');
+                res.end();
+             });
+             break;
         default: 
-        fs.readFile(__dirname + htmlDir+ path, function(err, data){
-            if (err){
-                return send404(res);
-            }
-            res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-            res.write(data, 'utf8');
-            res.end();
-        });
+            fs.readFile(__dirname + htmlDir+ path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+                res.write(data, 'utf8');
+                res.end();
+            });
     }
 }),
 
@@ -62,11 +72,11 @@ io.set('log level', 1);
 
 // define interactions with client
 io.sockets.on('connection', function(socket){
-    
- socket.emit(common.SEND_ALL_ROUTES, {' narcis' : [44.4136738372583, 26.102828979492188, 44.4136738372583, 26.102828979492188, 
+
+   socket.emit(common.SEND_ALL_ROUTES, {' narcis' : [44.4136738372583, 26.102828979492188, 44.4136738372583, 26.102828979492188, 
     44.41408911082398, 26.10262580215931, 44.41425818906652, 26.10299527645111],
             'radu' : [ 44.4217124730134, 26.064776480197906, 44.42180059302007, 26.06495887041092, 44.42080444607848, 26.0658198595047]}); //Testing purposes; this should be taken out of the DB
- setInterval(function(){
+   setInterval(function(){
     socket.emit('date', {'date': new Date()});
 }, 1000);
     //recieve client data
@@ -84,9 +94,9 @@ io.sockets.on('connection', function(socket){
     });
     //receiveRoute
     socket.on(common.SEND_ROUTE, function(data){
-       process.stdout.write("received route\n");
-       process.stdout.write(data[0]+'');
-   });
+     process.stdout.write("received route\n");
+     process.stdout.write(data[0]+'');
+ });
     //routesRequest
     socket.on(common.REQUEST_ALL_ROUTES, function(data){
       process.stdout.write("received routes request\n");
