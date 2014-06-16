e729dd32ef42243960d5b8ba451dd675b11758f1
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-06-17 01:32:45 +0300

    markers with links to dynamic pages

---
+++ server/dbusageexample.js
@@ -0,0 +1,9 @@
+var db = require('./db');
+
+//db.createUser('andrei20pag', '123123', '1990-04-11 00:00:00', 'Baicoi', 'RO')
+//db.insertTraseu('narcis2', 'alergare usoara2', 22.3, 'seara','trotuar', 2, 4, 4, 'te mananca lupii', 'frumos', [44.41367383, 26.10282897, 44.41367383, 26.10282897, 
+//	44.414089110, 26.102625802, 44.414258189, 26.102995271]);
+
+db.getInfoTraseu(8,db.selectInfoCallback);
+//db.getTrasee(db.selectInfoCallback);
+
--- server/html/client.js
+++ server/html/client.js
@@ -232,22 +232,26 @@ function handleDirectionsResponse( response, status ){
         traseu.id = receivedRoutesDict[ key ][ 'id' ];
         traseu.name = receivedRoutesDict[ key ][ 'name' ];
         calcRouteFromScratch( traseu ); 
-        createMarker( traseu );
+        createMarker( traseu, "/pagina_traseu.html?id=" +  traseu.id );
       }
 
     }
 
-    function createMarker( traseu ){
+
+    function createMarker( traseu, url){
       
-      traseu.marker = new google.maps.Marker({
+      var marker = new google.maps.Marker({
           position : traseu.markers[0]
       });
-      traseu.marker.setMap(map);
+      marker.setMap(map);
       var infowindow=new google.maps.InfoWindow({
           content : traseu.name
       });//asigneaza textul in casuta
-      infowindow.open( map,traseu.marker );
-      google.maps.event.addListener(traseu.marker,'click',function(){window.open("/pagina_traseu.html","_self")});
+      infowindow.open( map, marker );
+      console.log(traseu.id);
+      console.log(url);
+      google.maps.event.addListener(marker,'click',function(){window.open(url,"_self")});
+      
     }
 
 
@@ -373,3 +377,29 @@ if (ales_circ != "" && ales_caini != "" && ales_lum != "" && cand_box.length>1 &
 
 
 }//închide funcția
+
+
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") {
+        return;
+    }
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=");
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) {
+            parms[n] = [];
+        }
+
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
