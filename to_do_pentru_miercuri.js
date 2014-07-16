82236311b7ff99c29a5e5c7a560900952b4b8dd7
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-07-17 00:16:46 +0300

    not needed

--- to_do_pentru_miercuri.js
+++
@@ -1,5 +0,0 @@
-TO DO pentru miercuri:
-1. Pus validare si pe traseu - daca nu s-au ales macar doua puncte, afisam mesaj prin clasa #eroare_rasp.
-2. Pus balonase pentru fiecare traseu.
-3. De pus link pe balonase. Eventual pagini dinamice.
-4. De scris in baza traseul+detaliile? (doar daca avem timp)
