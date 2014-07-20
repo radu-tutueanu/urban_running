365c774261d5907dd5cf27670290312b67c81f9b
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-07-20 17:15:21 +0300

    init Route class

---
+++ server/html/js/route.js
@@ -0,0 +1,16 @@
+funtion Route() {
+	this.routeJSON = {};
+	this.latLngCoordinates = [];
+}
+
+Route.prototype.getJSON = function() {
+	return RouteJSON;
+};
+
+Route.prototype.addLatLngCoordinate(coordLatLng) {
+	this.latLngCoordinates.push(coordLatLng);
+}
+
+Route.prototype.getLatLngCoordinates(coordLatLng) {
+	return this.latLngCoordinates;
+}
