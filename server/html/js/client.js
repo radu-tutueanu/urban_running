fdea574aabd2281e69635ae7c0cfea958aaea3d4
Radu Tutueanu <radu.tutueanu@gmail.com>
2014-08-12 21:16:44 +0300

    chore(pagina_traseu.html): use oop functions

--- server/html/js/client.js
+++ server/html/js/client.js
@@ -103,3 +103,29 @@ ClientUtilities.prototype.validateAndSave = function() {
 
 
 }
+
+ClientUtilities.prototype.parseURLParams = function(url) {
+  var queryStart = url.indexOf("?") + 1,
+    queryEnd = url.indexOf("#") + 1 || url.length + 1,
+    query = url.slice(queryStart, queryEnd - 1),
+    pairs = query.replace(/\+/g, " ").split("&"),
+    parms = {},
+    i, n, v, nv;
+
+  if (query === url || query === "") {
+    return;
+  }
+
+  for (i = 0; i < pairs.length; i++) {
+    nv = pairs[i].split("=");
+    n = decodeURIComponent(nv[0]);
+    v = decodeURIComponent(nv[1]);
+
+    if (!parms.hasOwnProperty(n)) {
+      parms[n] = [];
+    }
+
+    parms[n].push(nv.length === 2 ? v : null);
+  }
+  return parms;
+}
